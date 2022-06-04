f677190edd0b783be8979d9a269722b8495bdd3d
kanz1384 <kanz1384@gmail.com>
2022-06-04 13:02:47 +0430

    configured

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-project_name=newsletter
+project_name=newsletter-subscription
 echo "$project_name is deploying..."
 
 current_time=$(date "+%Y%m%d_%H%M%S")
 backup_dir=$HOME/backups/$project_name/backup_$current_time/
 
+mkdir -p "$HOME/artifacts/$project_name/app_files"
 mkdir -p "$backup_dir"
 
 docker-compose down && docker system prune -f && docker network prune -f
 echo "Old docker containers removed"
 
-tar -pczf "$backup_dir/project.tar.gz" --exclude .git .
+tar -pczf "$backup_dir/project.tar.gz" --exclude .git --exclude target .
 echo "Project backed up to $backup_dir"
 
 ./mvnw clean package -Dmaven.test.skip=true dockerfile:build -f newsletter-subscription
