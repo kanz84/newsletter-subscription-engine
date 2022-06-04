d40f94dbc55da8ead2d185e6d045c7392a901a08
kanz1384 <kanz1384@gmail.com>
2022-06-04 18:37:19 +0430

    get jdk on demand

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,17 @@
 project_name=newsletter-subscription
 echo "$project_name is deploying..."
 
+mkdir -p "$HOME/tmp/jdk"
+JDK="$HOME/tmp/jdk/jdk-11"
+if [ ! -d "$JDK" ]; then
+    wget -c -O $HOME/tmp/jdk/openjdk-11+28_linux-x64_bin.tar.gz https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
+    echo "JDK downloaded"
+    tar -xf $HOME/tmp/jdk/openjdk-11+28_linux-x64_bin.tar.gz -C $HOME/tmp/jdk/
+    echo "JDK extracted"
+else
+    echo "JDK already existed in $JDK"
+fi
+
 current_time=$(date "+%Y%m%d_%H%M%S")
 backup_dir=$HOME/backups/$project_name/backup_$current_time/
 
@@ -15,7 +26,7 @@ echo "Old docker containers removed"
 tar -pczf "$backup_dir/project.tar.gz" --exclude .git --exclude target .
 echo "Project backed up to $backup_dir"
 
-JAVA_HOME="/home/git-auto-deploy/jdk/jdk-11"  ./mvnw clean package install -Dmaven.test.skip=true dockerfile:build -f newsletter-subscription
+JAVA_HOME="$JDK"  ./mvnw clean package install -Dmaven.test.skip=true dockerfile:build -f newsletter-subscription
 
 docker-compose up -d --build
 echo "Docker image built"
